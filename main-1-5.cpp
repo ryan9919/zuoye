6cd8a37c0b4def42d6bda5ec6bb2a0f41f176505
ryan9919 <161264570+ryan9919@users.noreply.github.com>
2024-03-08 22:49:50 +1030

    Add files via upload

---
+++ main-1-5.cpp
@@ -0,0 +1,13 @@
+#include <function-1-5.cpp>
+int main() {
+    int matrix1[3][3] = {{0, 1, 2},
+                          {3, 4, 5},
+                          {6, 7, 8}};
+    int matrix2[3][3] = {{0, 0, 0},
+                          {2, 2, 2},
+                          {-5, -4, 8}};
+    
+    print_summed(matrix1, matrix2); // Call the function with the two matrices
+    
+    return 0;
+}
