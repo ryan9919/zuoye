1256e69533dd465fc5c67e5349130de5f1078174
ryan9919 <161264570+ryan9919@users.noreply.github.com>
2024-03-08 22:49:05 +1030

    Add files via upload

---
+++ main-1-2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int is_identity(int array[10][10]);
+
+int main() {
+    int matrix[10][10] = {{1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 1}};
+    
+    int result = is_identity(matrix);
+    
+    if (result == 1){
+        std::cout << "The matrix is a identity matrix" << result << std::endl;
+    }else {
+        std::cout << "The matrix is not a identity matrix" << result << std::endl;
+    }
+    return 0;
+}
